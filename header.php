cf64da7721cb8ab0495a0e924411f5a7274f184f
Mfrielink <martijndoei@live.nl>
2014-11-24 10:07:53 +0100

    Change HMTL to PHP
    Index.html is now Index.php
    Notebook.html is now Notebook.php
    Menu, Header and footer are now separate php-files

---
+++ header.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	
+    <div class="logo">Webshop</div> 
+
+    <div class="searchbox">
+	    <form>
+			<span><input type="text" class="search" placeholder="Search..."></span>
+		</form>
+    </div>
+
+    <div class="shoppingCart"><a href="shoppingCart.html">Shopping cart</a></div>
+
+</head>
+</html?
